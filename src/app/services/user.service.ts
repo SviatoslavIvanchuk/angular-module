29d8e24d782eac66436f86c90d0673de7744fcd6
SviatoslavIvanchuk <isv1796@gmail.com>
2022-05-17 19:15:36 +0300

    lesson1_ClassWork

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../interfaces/user.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<IUser[]> {
+    return this.http
+      .get<IUser[]>(this.url);
+  }
+
+  getUser(id: number): Observable<IUser> {
+    return this.http
+      .get<IUser>(this.url + '/' + id)
+  }
+}
